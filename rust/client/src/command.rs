2456736611ef11d33b224e3bba44140594be5e90
itsAnian <anian.seidl@gmail.com>
2025-08-23 23:09:39 +0200

    refactor: made the code better understandable

--- rust/client/src/command.rs
+++ rust/client/src/command.rs
@@ -2,6 +2,7 @@ use crate::app;
 use crossterm::terminal::disable_raw_mode;
 use shared::{ClientAction::Leave, ClientAction::Send, TalkProtocol};
 use std::{
+    num::ParseIntError,
     process,
     time::{SystemTime, UNIX_EPOCH},
 };
@@ -32,50 +33,91 @@ pub fn parse(app: &mut app::App) {
 }
 
 fn parse_command(app: &mut app::App) {
-    let mut com = TalkProtocol {
-        uuid: Uuid::new_v4(),
-        username: app.username.clone(),
-        message: Some("".to_string()),
-        action: Send,
-        room_id: app.room,
-        unixtime: get_unix_timestamp(),
-    };
-
     if app.input.starts_with("name") {
         app.input = app.input.trim_start_matches("name ").trim().to_string();
-        let old_username = app.username.to_string();
-        app.username = app.input.to_string();
-        let message = format!("{} changed his name to '{}'", old_username, app.username);
-        com.username = "Info".to_string();
-        com.message = Some(message.to_string());
+        let com = parse_command_name(app);
         app.tx.unbounded_send(com).unwrap();
     } else if app.input.starts_with("room") {
         app.input = app.input.trim_start_matches("room").trim().to_string();
         match app.input.parse::<i32>() {
             Ok(number) => {
-                app.room = number;
-                let message = format!("{} changed to room {}", app.username, app.room);
-                com.username = "Info".to_string();
-                com.message = Some(message.to_string());
+                let com = parse_command_room_valid(app, number);
                 app.tx.unbounded_send(com).unwrap();
             }
             Err(error) => {
-                com.username = "Error".to_string();
-                com.message = Some(error.to_string());
+                let com = parse_command_room_invalid(app, error);
                 app.communication.lock().unwrap().push(com);
             }
         }
     } else if app.input.starts_with("broadcast") {
         app.input = app.input.trim_start_matches("broadcast").trim().to_string();
-        com.username = "Broadcast".to_string();
-        com.message = Some(app.input.to_string());
+        let com = parse_command_broadcast(app);
         app.tx.unbounded_send(com).unwrap();
     } else if app.input == "clear" {
         app.communication.lock().unwrap().clear();
     } else {
-        com.username = "Error".to_string();
-        let message = format!("The command '{}' does not exist", app.input);
-        com.message = Some(message.to_string());
+        let com = parse_invalid_command(app);
         app.communication.lock().unwrap().push(com);
+    }
+}
+
+fn parse_command_room_valid(app: &mut app::App, number: i32) -> TalkProtocol {
+    app.room = number;
+    TalkProtocol {
+        uuid: Uuid::new_v4(),
+        username: "Info".to_string(),
+        message: Some(format!("{} changed to room {}", app.username, app.room)),
+        action: Send,
+        room_id: app.room,
+        unixtime: get_unix_timestamp(),
+    }
+}
+
+fn parse_command_room_invalid(app: &mut app::App, error: ParseIntError) -> TalkProtocol {
+    TalkProtocol {
+        uuid: Uuid::new_v4(),
+        username: "Error".to_string(),
+        message: Some(error.to_string()),
+        action: Send,
+        room_id: app.room,
+        unixtime: get_unix_timestamp(),
+    }
+}
+
+fn parse_command_name(app: &mut app::App) -> TalkProtocol {
+    let old_username = app.username.to_string();
+    app.username = app.input.to_string();
+    TalkProtocol {
+        uuid: Uuid::new_v4(),
+        username: "Info".to_string(),
+        message: Some(format!(
+            "{} changed his name to '{}'",
+            old_username, app.username
+        )),
+        action: Send,
+        room_id: app.room,
+        unixtime: get_unix_timestamp(),
+    }
+}
+
+fn parse_invalid_command(app: &mut app::App) -> TalkProtocol {
+    TalkProtocol {
+        uuid: Uuid::new_v4(),
+        username: "Error".to_string(),
+        message: Some(format!("The command '{}' does not exist", app.input)),
+        action: Send,
+        room_id: app.room,
+        unixtime: get_unix_timestamp(),
+    }
+}
+
+fn parse_command_broadcast(app: &mut app::App) -> TalkProtocol {
+    TalkProtocol {
+        uuid: Uuid::new_v4(),
+        username: "Broadcast".to_string(),
+        message: Some(app.input.to_string()),
+        action: Send,
+        room_id: app.room,
+        unixtime: get_unix_timestamp(),
     }
 }

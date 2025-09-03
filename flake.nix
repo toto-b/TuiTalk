de472819996d1a6bc8cdb95e16afdb762c80e5a1
itsAnian <anian.seidl@gmail.com>
2025-09-03 19:08:35 +0200

    feat: add nix run to flake

--- flake.nix
+++ flake.nix
@@ -9,31 +9,48 @@
     };
   };
 
-  outputs = { self, nixpkgs, treefmt-nix }:
-    let pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [
-          rustc
-          cargo
-          trunk
-          rustfmt
-          clippy
-          bacon
-          rust-analyzer
-          lld_18
-          wasm-bindgen-cli
-          python3
-          wasm-pack
-          redis
-        ];
-      };
-
-      formatter.x86_64-linux =
-        treefmt-nix.lib.mkWrapper nixpkgs.legacyPackages.x86_64-linux {
-          projectRootFile = "flake.nix";
-          programs.nixpkgs-fmt.enable = true;
-          programs.rustfmt.enable = true;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    treefmt-nix,
+  }: let
+    pkgs = import nixpkgs {system = "x86_64-linux";};
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        rustc
+        cargo
+        trunk
+        rustfmt
+        clippy
+        bacon
+        rust-analyzer
+        lld_18
+        wasm-bindgen-cli
+        python3
+        wasm-pack
+        redis
+      ];
     };
+
+    formatter.x86_64-linux = treefmt-nix.lib.mkWrapper nixpkgs.legacyPackages.x86_64-linux {
+      projectRootFile = "flake.nix";
+      programs.nixpkgs-fmt.enable = true;
+      programs.rustfmt.enable = true;
+    };
+
+    packages.x86_64-linux.client = pkgs.rustPlatform.buildRustPackage {
+      pname = "client";
+      version = "0.1.0";
+      src = ./rust;
+      cargoLock.lockFile = ./rust/Cargo.lock;
+
+      cargoBuildFlags = ["-p" "client"];
+
+      nativeBuildInputs = with pkgs; [pkg-config perl];
+      buildInputs = with pkgs; [openssl];
+    };
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.client;
+  };
 }

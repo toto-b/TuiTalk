c50c39e8f70e37fa8f48f7a45abeae5c291da2e9
itsAnian <anian.seidl@gmail.com>
2025-09-05 12:30:03 +0200

    refactor: unwraps; outsource get_first_message_timestamp

--- rust/client/src/command.rs
+++ rust/client/src/command.rs
@@ -15,14 +15,35 @@ pub fn get_unix_timestamp() -> u64 {
     now.as_secs()
 }
 
+pub fn get_first_message_timestamp(app: &mut app::App) -> u64 {
+    app.communication
+        .lock()
+        .expect("Vector of communication")
+        .iter()
+        .find_map(|proto| match proto {
+            TalkProtocol::Error { .. } => None,
+            TalkProtocol::LocalError { .. } => None,
+            TalkProtocol::PostMessage { message } => Some(message.unixtime),
+            TalkProtocol::UserJoined { unixtime, .. } => Some(*unixtime),
+            TalkProtocol::UserLeft { unixtime, .. } => Some(*unixtime),
+            TalkProtocol::UsernameChanged { unixtime, .. } => Some(*unixtime),
+            _ => None,
+        })
+        .unwrap_or(get_unix_timestamp())
+}
+
 pub fn join_initial_room(app: &mut app::App) {
     let com = join_room(app);
-    app.tx.unbounded_send(com).unwrap();
+    app.tx
+        .unbounded_send(com)
+        .expect("Successfully transmitted");
 }
 
 pub fn quit_app(app: &mut app::App) {
     let com = leave_room(app);
-    app.tx.unbounded_send(com).unwrap();
+    app.tx
+        .unbounded_send(com)
+        .expect("Successfully transmitted");
 }
 
 pub fn join_room(app: &mut app::App) -> TalkProtocol {
@@ -47,7 +68,9 @@ pub fn parse(app: &mut app::App) {
     if app.input.is_empty() {
     } else if app.input.len() >= MESSAGE_LENGTH {
         let com = parse_message_too_long();
-        app.communication.lock().unwrap().push(com);
+        app.tx
+            .unbounded_send(com)
+            .expect("Successfully transmitted");
     } else if app.input.starts_with("/") {
         app.input = app.input.trim_start_matches("/").trim().to_string();
         parse_command(app);
@@ -61,7 +84,9 @@ pub fn parse(app: &mut app::App) {
                 unixtime: get_unix_timestamp(),
             },
         };
-        app.tx.unbounded_send(com).unwrap();
+        app.tx
+            .unbounded_send(com)
+            .expect("Successfully transmitted");
     }
 }
 
@@ -70,42 +95,68 @@ fn parse_command(app: &mut app::App) {
         app.input = app.input.trim_start_matches("name ").trim().to_string();
         if app.input.len() <= USERNAME_LENGTH {
             let com = parse_command_name(app);
-            app.tx.unbounded_send(com).unwrap();
+            app.tx
+                .unbounded_send(com)
+                .expect("Successfully transmitted");
         } else {
             let com = parse_message_too_long();
-            app.communication.lock().unwrap().push(com);
+            app.communication
+                .lock()
+                .expect("Communication Vector")
+                .push(com);
         }
     } else if app.input.starts_with("room") {
         app.input = app.input.trim_start_matches("room").trim().to_string();
         match app.input.parse::<i32>() {
             Ok(number) => {
                 let (leave, join) = parse_command_room_valid(app, number);
-                app.tx.unbounded_send(join).unwrap();
-                app.communication.lock().unwrap().clear();
-                app.tx.unbounded_send(leave).unwrap();
+                app.tx
+                    .unbounded_send(join)
+                    .expect("Successfully transmitted");
+                app.communication
+                    .lock()
+                    .expect("Communication Vector")
+                    .clear();
+                app.tx
+                    .unbounded_send(leave)
+                    .expect("Successfully transmitted");
             }
             Err(error) => {
                 let com = parse_command_room_invalid(error);
-                app.communication.lock().unwrap().push(com);
+                app.communication
+                    .lock()
+                    .expect("Communication Vector")
+                    .push(com);
             }
         }
     } else if app.input == "clear" {
-        app.communication.lock().unwrap().clear();
+        app.communication
+            .lock()
+            .expect("Communication Vector")
+            .clear();
     } else if app.input.starts_with("fetch") {
         app.input = app.input.trim_start_matches("fetch").trim().to_string();
         match app.input.parse::<i64>() {
             Ok(number) => {
                 let com = parse_command_fetch_valid(app, number);
-                app.tx.unbounded_send(com).unwrap();
+                app.tx
+                    .unbounded_send(com)
+                    .expect("Successfully transmitted");
             }
             Err(error) => {
                 let com = parse_command_fetch_invalid(error);
-                app.communication.lock().unwrap().push(com);
+                app.communication
+                    .lock()
+                    .expect("Communication Vector")
+                    .push(com);
             }
         }
     } else {
         let com = parse_invalid_command(app);
-        app.communication.lock().unwrap().push(com);
+        app.communication
+            .lock()
+            .expect("Communication Vector")
+            .push(com);
     }
 }
 
@@ -143,21 +194,7 @@ fn parse_command_fetch_valid(app: &mut app::App, set_limit: i64) -> TalkProtocol
     TalkProtocol::Fetch {
         room_id: app.room,
         limit: set_limit,
-        fetch_before: app
-            .communication
-            .lock()
-            .unwrap()
-            .iter()
-            .find_map(|proto| match proto {
-                TalkProtocol::Error { .. } => None,
-                TalkProtocol::LocalError { .. } => None,
-                TalkProtocol::PostMessage { message } => Some(message.unixtime),
-                TalkProtocol::UserJoined { unixtime, .. } => Some(*unixtime),
-                TalkProtocol::UserLeft { unixtime, .. } => Some(*unixtime),
-                TalkProtocol::UsernameChanged { unixtime, .. } => Some(*unixtime),
-                _ => None,
-            })
-            .unwrap_or(get_unix_timestamp()),
+        fetch_before: get_first_message_timestamp(app),
     }
 }
 
